1eeaf9ab954f728588ecbb9156d7e05e9cf1a62d
samvarsh <varshnisampath@gmail.com>
2021-11-16 23:02:30 -0800

    Update sieve3.c

--- sieve3.c
+++ sieve3.c
@@ -56,6 +56,88 @@ int main (int argc, char *argv[])
       well as the integers represented by the first and
       last array elements */
 
+   low_value = 3 +  2 * (id*((n/2)-1)/p);
+   high_value = 1 + 2 * ((id+1)*((n/2)-1)/p);
+   size = ((high_value - low_value)/2) + 1;
+
+   local_prime_size = (long long) sqrt((double) n);
+   proc0_size = ((n/2)-1)/p;
+
+   if (( (2 * proc0_size) + 3) < (long long) sqrt((double) n)/2) {
+      if (!id) printf ("Too many processes\n");
+      MPI_Finalize();
+      exit (1);
+   }
+
+   marked = (char *) malloc (size);
+	
+   prime_marked = (char *) malloc (local_prime_size);
+
+   if (marked == NULL || prime_marked == NULL) {
+      printf ("Cannot allocate enough memory\n");
+      MPI_Finalize();
+      exit (1);
+   }
+
+   for (i = 0; i < size; i++) 
+	   marked[i] = 0;
+
+   for (i = 0; i < local_prime_size; i++) 
+	   prime_marked[i] = 0;   
+
+
+   cache_size = 1<<19;
+   last = 0;
+
+   do {
+   index=0;
+   prime = 3;
+
+   	do {
+      		if (prime * prime > low_value)
+         		first = (prime * prime - low_value)/2;
+      		else {
+         		if (!(low_value % prime)) first = 0;
+         		else  {
+            			first = prime - (low_value % prime);
+            			if ( (low_value+first)%2 == 0  ) {
+               				first = first + prime;
+            		        }
+                                first = first / 2 ;
+                        }
+                }
+
+
+      		prime_first = (prime * prime -3)/2; 
+			
+			// Marking the required primes - done sequentially - that is by all processes
+      		for (i = prime_first; i<local_prime_size; i += prime) 
+				prime_marked[i] = 1; 
+            // Marking in the set space - cache size
+      		for (i = first + last ; i < MIN(last + cache_size,size); i += prime)
+				marked[i] = 1;
+             
+
+
+      		while (prime_marked[++index]);
+         		prime = 2 * index + 3;
+
+   	} while (prime * prime <= high_value);
+	
+	low_value = low_value + cache_size * 2;
+        last = last + cache_size; 
+		
+   } while (last<size);
+
+   count = 0;
+   global_count = 0;
+
+   for (i = 0; i < size; i++)
+      if (!marked[i]) count++;
+
+   MPI_Reduce (&count, &global_count, 1, MPI_INT, MPI_SUM,
+                          0, MPI_COMM_WORLD);
+
    /* Stop the timer */
 
    elapsed_time += MPI_Wtime();
